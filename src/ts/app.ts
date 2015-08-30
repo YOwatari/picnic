0ff9bdd639e6006b39fcae29e86eddcd808fb7c5
Yuta OHWATARI <yuta_ohwatari@voyagegroup.com>
2015-08-30 22:51:34 +0900

    Use import statement

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,25 +1,28 @@
 /// <reference path="../typings/github-electron/github-electron.d.ts" />
 /// <reference path="../typings/node/node.d.ts" />
 
-var app: GitHubElectron.App = require('app');
-var BrowserWindow = require('browser-window');
+import app from 'app';
+import BrowserWindow from 'browser-window';
+import crashReporter from 'crash-reporter';
 
-require('crash-reporter').start();
+let mainWindow: BrowserWindow = null;
+
+// for development
+if (process.env.NODE_ENV === 'develop') {
+    crashReporter.start();
+}
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
     app.quit();
-  }
 });
 
 app.on('ready', () => {
-  var mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
   mainWindow.loadUrl('file://' + __dirname + '/../index.html');
-
-  mainWindow.openDevTools();
 
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
 });
+
